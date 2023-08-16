1f2e5a82a9b6a93827bef7424cf547565c4de388
Stephen Beckstrom-Sternberg <Stephen.Beckstrom-Sternberg@azdhs.gov>
2023-08-16 11:34:45 -0700

    Add files via upload
    HeatCluster v0.4.8.

--- HeatCluster.py
+++ HeatCluster.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ##########################################
-# HeatCluster-0.4.4                       #
+# HeatCluster-0.4.8                       #
 # written by Stephen Beckstrom-Sternberg  #
 # Creates SNP heat/cluster maps           #
 # from SNP matrices                       #
@@ -18,163 +18,150 @@ from sklearn.metrics import silhouette_score
 from io import StringIO
 from pathlib import Path
 
-# Read the SNP matrix file and determine the delimiter
-path = Path('./snp-dists.txt')
-try:
-    path.resolve(strict=True)
-except FileNotFoundError:
-    path = Path('./snp_matrix.txt')
+def read_snp_matrix(path):
+    try:
+        path.resolve(strict=True)
+    except FileNotFoundError:
+        path = Path('./snp_matrix.txt')
+    print("Using file path:", path)
 
-print("Using file path:", path)
+    with open(path, "r") as infile:
+        lines = infile.readlines()
+    
+    return lines
 
-# Read the SNP matrix file into a DataFrame
-with open(path, "r") as infile:
-    lines = infile.readlines()  # Do NOT Skip the header line
+def clean_and_read_df(lines):
+    """
+    Clean and read DataFrame from lines.
+    
+    Args:
+        lines (list): List of strings representing lines of data.
+        
+    Returns:
+        df (DataFrame): Cleaned DataFrame.
+    """
+    # Join lines into a single string
+    snp_matrix_string = "".join(lines)
+    
+    # Read DataFrame from string
+    df = pd.read_csv(StringIO(snp_matrix_string), sep=',')
+    
+    # Convert column names to strings
+    df.columns = df.columns.map(str)
+    
+    # Define consensus patterns
+    consensus_patterns = ['snp-dists 0.8.2', '.consensus_threshold_0.6_quality_20', 'Consensus_']
+    
+    # Replace consensus patterns in column names
+    df.columns = df.columns.str.replace('|'.join(consensus_patterns), '', regex=True)
+    # Replace consensus patterns in entire dataframe to change row names
+    df = df.replace(consensus_patterns, '', regex=True) 
 
-numSamples = len(lines) - 1  # counts data lines
+    # Keep only numeric columns
+    df = df.set_index(df.columns[0])
+    df.dropna(axis=0, inplace=True)
+    df.dropna(axis=1, inplace=True)  
+    return df
 
-# Combine the cleaned lines into a single string instead of a file
-snp_matrix_string = "\n".join(line for line in lines)
+def compute_clusters(data, method='complete'):
+    clusters = sch.linkage(data.values, method=method, metric='euclidean')
+    return clusters
 
-print("\n\n", snp_matrix_string, "\n\n")
+def calculate_silhouette_scores(data, upper_range):
+    silhouette_scores = []
+    for n_clusters in range(2, upper_range):
+        kmeans = KMeans(n_clusters=n_clusters, n_init=10)
+        cluster_labels = kmeans.fit_predict(data.values)
+        silhouette_avg = silhouette_score(data.values, cluster_labels)
+        silhouette_scores.append(silhouette_avg)
+#        print(silhouette_scores,"\n") 
+    return silhouette_scores
 
-# Read the tab-delimited string into a DataFrame
-df = pd.read_csv(StringIO(snp_matrix_string), sep=',')
+def main():
+    path = Path('./snp-dists.txt')
+    lines = read_snp_matrix(path)
+    numSamples = len(lines) - 1
 
+    df = clean_and_read_df(lines)
 
-# Remove 'snp-dists 0.8.2,' from the first column name
-# Define the regular expression patterns for replacements
-consensus_pattern = r'snp-dists 0\.8\.2,|\.consensus_threshold_0\.6_quality_20|Consensus_'
+    # Set output figure size tuple based on number of samples
+    if numSamples <= 20:
+        figureSize = (10, 8)
+    elif numSamples <= 40:
+        figureSize = (20, 16)
+    elif numSamples <= 60:
+        figureSize = (30, 24)
+    else:
+        figureSize = (40, 32)
+    
+    df = df.loc[df.sum(axis=1).sort_values(ascending=True).index]
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df.dropna(inplace=True)
 
-# Replace the header row using the pattern
-df.columns = df.columns.str.replace(consensus_pattern, '', regex=True)
+    clusters = compute_clusters(df)
+    clustergrid = sns.clustermap(
+        df,
+        xticklabels=True,
+        yticklabels=True,
+        vmin=0,
+        vmax=50,
+        center=20,
+        annot=True,
+        annot_kws={'size': 6},
+        cbar_kws={"pad": 0.5},
+        cmap='Reds_r',
+        linecolor="white",
+        linewidths=.2,
+        fmt='d',
+        dendrogram_ratio=0.1,
+        col_cluster=True,
+        row_cluster=True,
+        figsize=figureSize
+    )
+    plt.setp(clustergrid.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(clustergrid.ax_heatmap.get_yticklabels(), rotation='horizontal')
+    clustergrid.ax_row_dendrogram.set_visible(False)
+    clustergrid.ax_col_dendrogram.set_visible(True)
 
-# Replace the data rows using the same pattern
-df = df.replace(consensus_pattern, '', regex=True)
+    row_order = clustergrid.dendrogram_row.reordered_ind
+    col_order = clustergrid.dendrogram_col.reordered_ind
 
-df = df.set_index(df.columns[0])
+# Use the reordered indices to sort the DataFrame
+    sorted_df = df.iloc[row_order, col_order]
 
-print("/n/n", df, "\n\n")
-# Define colormap for heatmap
-cmap = 'Reds_r'
-print("Colormap set to '", cmap, "'")
+    df = sorted_df.apply(lambda row: row[row < 500].sum(), axis=1)
+    sorted_df['Within_Cluster_SNPs'] = df.values
 
-# Vectorized operation to add 'Total_SNPs' column
-df['Total_SNPs'] = df.sum(axis=1)
+    silhouette_scores = calculate_silhouette_scores(sorted_df, min(numSamples, 11))
+    optimal_num_clusters = silhouette_scores.index(max(silhouette_scores)) + 2
 
-# Vectorized operation to sort based on 'Total_SNPs'
-df = df.sort_values(by='Total_SNPs', kind='mergesort', axis=0)
-
-# Reorder the columns to mirror the row order
-sorted_cluster_matrix = df.reindex(columns=df.index)
-
-# Check for Missing Values
-if sorted_cluster_matrix.isnull().sum().sum() > 0:
-    sorted_cluster_matrix = sorted_cluster_matrix.dropna()
-
-# Check for Non-Finite Values
-if ~np.isfinite(sorted_cluster_matrix).all().all():
-    sorted_cluster_matrix = sorted_cluster_matrix.replace([np.inf, -np.inf], np.nan).dropna()
-
-# Change output figure size tuple based on number of samples
-if numSamples <= 20:
-    figureSize = (10, 8)
-elif numSamples <= 40:
-    figureSize = (20, 16)
-elif numSamples <= 60:
-    figureSize = (30, 24)
-else:
-    figureSize = (40, 32)
-print("Number of samples:", numSamples, "\nFigure size:", figureSize)
-
-# Compute clusters
-# Compute clusters using fastcluster
-#clusters = fastcluster.linkage(sorted_cluster_matrix.values, method='complete', metric='euclidean')
-clusters = sch.linkage(sorted_cluster_matrix.values, method='complete', metric='euclidean')
-
-# Create clustermap to get the order of rows and columns based on clustering
-clustergrid = sns.clustermap(
-    sorted_cluster_matrix,
-    xticklabels=True,
-    vmin=0,
-    vmax=50,
-    center=20,
-    annot=True,
-    annot_kws={'size': 6},
-    cbar_kws={"pad": 0.5},
-    cmap=cmap,
-    linecolor="white",
-    linewidths=.2,
-    fmt='d',
-    dendrogram_ratio=0.1,
-    col_cluster=True,
-    row_cluster=True,
-    figsize=figureSize
-)
-plt.setp(clustergrid.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
-
-# Suppress printing of dendrogram along the y-axis
-clustergrid.ax_row_dendrogram.set_visible(False)
-clustergrid.ax_col_dendrogram.set_visible(True)
-
-row_order = clustergrid.dendrogram_row.reordered_ind
-col_order = row_order
-
-# Sort the DataFrame based on the cluster order
-sorted_df = sorted_cluster_matrix.iloc[row_order, col_order]
-
-# Vectorized operation to calculate 'within_cluster_snps'
-within_cluster_snps = (df[df < 500]).sum(axis=1)
-
-# Computing the number of SNPs within the cluster per row
-within_cluster_snps = sorted_df.apply(lambda row: row[row < 500].sum(), axis=1)
-
-# Add 'Within_Cluster_SNPs' column to the sorted DataFrame
-sorted_df['Within_Cluster_SNPs'] = within_cluster_snps.values
-
-# Calculate silhouette scores for different numbers of clusters
-silhouette_scores = []
-upper_range = min(numSamples, 11)
-for n_clusters in range(2, upper_range):
-    kmeans = KMeans(n_clusters=n_clusters, n_init=10)
+    kmeans = KMeans(n_clusters=optimal_num_clusters, n_init=10)
     cluster_labels = kmeans.fit_predict(sorted_df.values)
-    silhouette_avg = silhouette_score(sorted_df.values, cluster_labels)
-    silhouette_scores.append(silhouette_avg)
+    sorted_df['Cluster'] = cluster_labels
 
-# Find the optimal number of clusters with the highest silhouette score
-optimal_num_clusters = silhouette_scores.index(max(silhouette_scores)) + 2
+    sorted_df = sorted_df.sort_values(by=['Cluster', 'Within_Cluster_SNPs'], ascending=[True, True], kind="mergesort")
+    print("sorted_df\n",sorted_df,"\n\n")
+    sorted_df.drop(['Cluster', 'Within_Cluster_SNPs'], axis='columns', inplace=True)
+    sorted_df = sorted_df.reindex(columns=sorted_df.index)
 
-# Use the optimal number of clusters to assign cluster labels and sort the DataFrame
-kmeans = KMeans(n_clusters=optimal_num_clusters, n_init=10)
-cluster_labels = kmeans.fit_predict(sorted_df.values)
-sorted_df['Cluster'] = cluster_labels
+    heatmap = sns.clustermap(
+        sorted_df, xticklabels=True, yticklabels=True, vmin=0, vmax=50, center=20,
+        annot=True, annot_kws={'size': 6}, cbar_kws={"orientation": "vertical", "pad": 0.5},
+        cmap='Reds_r', linecolor="white", linewidths=.1, fmt='d', dendrogram_ratio=0.1,
+        col_cluster=True, row_cluster=True,
+    )
+# Set orientation of axes labels
+    plt.setp(heatmap.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(heatmap.ax_heatmap.get_yticklabels(), rotation='horizontal')
+    
+    heatmap.ax_row_dendrogram.set_visible(False)
+    heatmap.ax_col_dendrogram.set_visible(True)
 
-# Sort the DataFrame first by 'Cluster' and then by 'Within_Cluster_SNPs'
-sorted_df = sorted_df.sort_values(by=['Cluster', 'Within_Cluster_SNPs'], ascending=[True, True], kind="mergesort")
+    heatmap.savefig('SNP_matrix.pdf')
+    heatmap.savefig('SNP_matrix.png')
 
-# Drop 'Cluster' and 'Within_Cluster_SNPs' columns
-sorted_df = sorted_df.drop(['Cluster', 'Within_Cluster_SNPs'], axis='columns')
-sorted_df = sorted_df.reindex(columns=sorted_df.index)
+    plt.show()
+    print("Done")
 
-# Save the finally sorted tab-delimited SNP matrix
-sorted_df.to_csv('Final_snp_matrix.tsv', sep='\t', header=True, index=True)
-
-# Create the reordered heatmap with correct values
-heatmap = sns.clustermap(
-    sorted_df, xticklabels=True, yticklabels=True, vmin=0, vmax=50, center=20,
-    annot=True, annot_kws={'size': 6}, cbar_kws={"orientation": "vertical", "pad": 0.5},
-    cmap=cmap, linecolor="white", linewidths=.1, fmt='d', dendrogram_ratio=0.1,
-    col_cluster=True, row_cluster=True, figsize=figureSize
-)
-plt.setp(heatmap.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
-
-heatmap.ax_row_dendrogram.set_visible(False)
-heatmap.ax_col_dendrogram.set_visible(True)
-
-# Save the reordered heatmap as PDF and PNG
-heatmap.savefig('SNP_matrix.pdf')
-heatmap.savefig('SNP_matrix.png')
-
-plt.show()
-plt.close()
-print("Done")
+if __name__ == "__main__":
+    main()
