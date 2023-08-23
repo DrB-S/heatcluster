aee58651fb31726242cbccfd132895edbfe54710
Erin Young <erin.olde@gmail.com>
2023-08-23 15:45:15 -0600

    added usage to readme

--- HeatCluster.py
+++ HeatCluster.py
@@ -1,51 +1,55 @@
 #!/usr/bin/python3
 
 ##########################################
-# HeatCluster-0.4.8                       #
+# HeatCluster-0.4.9                       #
 # written by Stephen Beckstrom-Sternberg  #
 # Creates SNP heat/cluster maps           #
 # from SNP matrices                       #
 ###########################################
 
-import re
+import argparse
+import fastcluster as sch 
+import logging
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-import fastcluster as sch 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-from io import StringIO
-from pathlib import Path
 
-def read_snp_matrix(path):
-    try:
-        path.resolve(strict=True)
-    except FileNotFoundError:
-        path = Path('./snp_matrix.txt')
-    print("Using file path:", path)
+#logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%y-%b-%d %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%y-%b-%d %H:%M:%S', level=logging.DEBUG)
 
-    with open(path, "r") as infile:
-        lines = infile.readlines()
-    
-    return lines
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', required = True, type = str, help ='input SNP matrix')
+parser.add_argument('-o', '--out', type=str, help='final file name', default='SNP_matrix')
+parser.add_argument('-t', '--type', type=str, help='file extention for final image', default = 'pdf')
+parser.add_argument('-v', '--version', help='print version and exit', action='version', version='%(prog)s ' + '0.4.9')
+args = parser.parse_args()
 
-def clean_and_read_df(lines):
+def read_snp_matrix(file):
+    logging.debug('Determining if file is comma or tab delimited')
+    tabs   = pd.read_csv(file, nrows=1, sep='\t').shape[1]
+    commas = pd.read_csv(file, nrows=1, sep=',').shape[1]
+    if tabs > commas:
+        logging.debug('The file is probaby tab-delimited')
+        df = pd.read_csv(file, sep='\t', index_col= False)
+    else:
+        logging.debug('The file is probaby comma-delimited')
+        df = pd.read_csv(file, sep=',', index_col= False)
+
+    return df
+
+def clean_and_read_df(df):
     """
     Clean and read DataFrame from lines.
     
-    Args:
-        lines (list): List of strings representing lines of data.
-        
     Returns:
         df (DataFrame): Cleaned DataFrame.
     """
-    # Join lines into a single string
-    snp_matrix_string = "".join(lines)
-    
-    # Read DataFrame from string
-    df = pd.read_csv(StringIO(snp_matrix_string), sep=',')
-    
+    logging.debug('Dropping the first column')
+    df = df.iloc[: , 1:]
+
     # Convert column names to strings
     df.columns = df.columns.map(str)
     
@@ -54,17 +58,15 @@ def clean_and_read_df(lines):
     
     # Replace consensus patterns in column names
     df.columns = df.columns.str.replace('|'.join(consensus_patterns), '', regex=True)
-    # Replace consensus patterns in entire dataframe to change row names
-    df = df.replace(consensus_patterns, '', regex=True) 
+    
+    # Setting the index
+    df = df.set_index(df.columns)
 
-    # Keep only numeric columns
-    df = df.set_index(df.columns[0])
-    df.dropna(axis=0, inplace=True)
-    df.dropna(axis=1, inplace=True)  
     return df
 
 def compute_clusters(data, method='complete'):
     clusters = sch.linkage(data.values, method=method, metric='euclidean')
+    logging.debug('The clusters are ' + str(clusters))
     return clusters
 
 def calculate_silhouette_scores(data, upper_range):
@@ -74,15 +76,28 @@ def calculate_silhouette_scores(data, upper_range):
         cluster_labels = kmeans.fit_predict(data.values)
         silhouette_avg = silhouette_score(data.values, cluster_labels)
         silhouette_scores.append(silhouette_avg)
-#        print(silhouette_scores,"\n") 
+
+    logging.debug('The silhouette scores are ' + str(silhouette_scores))
     return silhouette_scores
 
 def main():
-    path = Path('./snp-dists.txt')
-    lines = read_snp_matrix(path)
-    numSamples = len(lines) - 1
+    SNPmatrix=args.input
+    logging.info('Creating figure for ' + SNPmatrix)
 
-    df = clean_and_read_df(lines)
+    df = read_snp_matrix(SNPmatrix)
+    logging.debug('The input SNP matrix:')
+    logging.debug(df)
+
+    df = clean_and_read_df(df)
+    logging.debug('The clean SNP matrix:')
+    logging.debug(df)
+
+    numSamples = len(df.columns)
+    logging.info('Found ' + str(numSamples) + ' samples in ' + SNPmatrix)
+
+    if numSamples <= 3:
+        logging.fatal('This matrix has too few samples or has been melted. Sorry!')
+        exit(0)
 
     # Set output figure size tuple based on number of samples
     if numSamples <= 20:
@@ -93,12 +108,18 @@ def main():
         figureSize = (30, 24)
     else:
         figureSize = (40, 32)
+
+    logging.debug('The figure size will be ' + str(figureSize))
     
     df = df.loc[df.sum(axis=1).sort_values(ascending=True).index]
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    df.dropna(inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df = df.dropna()
 
+    #
+    # what is this used for? The clusters variable isn't used anywhere
     clusters = compute_clusters(df)
+    #
+
     clustergrid = sns.clustermap(
         df,
         xticklabels=True,
@@ -108,9 +129,9 @@ def main():
         center=20,
         annot=True,
         annot_kws={'size': 6},
-        cbar_kws={"pad": 0.5},
+        cbar_kws={'pad': 0.5},
         cmap='Reds_r',
-        linecolor="white",
+        linecolor='white',
         linewidths=.2,
         fmt='d',
         dendrogram_ratio=0.1,
@@ -121,12 +142,12 @@ def main():
     plt.setp(clustergrid.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
     plt.setp(clustergrid.ax_heatmap.get_yticklabels(), rotation='horizontal')
     clustergrid.ax_row_dendrogram.set_visible(False)
-    clustergrid.ax_col_dendrogram.set_visible(True)
+    clustergrid.ax_col_dendrogram.set_visible(False)
 
     row_order = clustergrid.dendrogram_row.reordered_ind
     col_order = clustergrid.dendrogram_col.reordered_ind
 
-# Use the reordered indices to sort the DataFrame
+    logging.debug('Use the reordered indices to sort the DataFrame')
     sorted_df = df.iloc[row_order, col_order]
 
     df = sorted_df.apply(lambda row: row[row < 500].sum(), axis=1)
@@ -139,29 +160,42 @@ def main():
     cluster_labels = kmeans.fit_predict(sorted_df.values)
     sorted_df['Cluster'] = cluster_labels
 
-    sorted_df = sorted_df.sort_values(by=['Cluster', 'Within_Cluster_SNPs'], ascending=[True, True], kind="mergesort")
-    print("sorted_df\n",sorted_df,"\n\n")
-    sorted_df.drop(['Cluster', 'Within_Cluster_SNPs'], axis='columns', inplace=True)
+    sorted_df = sorted_df.sort_values(by=['Cluster', 'Within_Cluster_SNPs'], ascending=[True, True], kind='mergesort')
+    sorted_df = sorted_df.drop(['Cluster', 'Within_Cluster_SNPs'], axis='columns')
     sorted_df = sorted_df.reindex(columns=sorted_df.index)
 
     heatmap = sns.clustermap(
-        sorted_df, xticklabels=True, yticklabels=True, vmin=0, vmax=50, center=20,
-        annot=True, annot_kws={'size': 6}, cbar_kws={"orientation": "vertical", "pad": 0.5},
-        cmap='Reds_r', linecolor="white", linewidths=.1, fmt='d', dendrogram_ratio=0.1,
-        col_cluster=True, row_cluster=True,
+        sorted_df, 
+        xticklabels=True, 
+        yticklabels=True, 
+        vmin=0, 
+        vmax=50, 
+        center=20,
+        annot=True, 
+        annot_kws={'size': 6}, 
+        cbar_kws={'orientation': 'vertical', 'pad': 0.5},
+        cmap='Reds_r', 
+        linecolor='white', 
+        linewidths=.1, 
+        fmt='d', 
+        dendrogram_ratio=0.1,
+        col_cluster=True, 
+        row_cluster=True,
     )
-# Set orientation of axes labels
+
+    logging.debug('Set orientation of axes labels')
     plt.setp(heatmap.ax_heatmap.get_xticklabels(), rotation=45, ha='right')
     plt.setp(heatmap.ax_heatmap.get_yticklabels(), rotation='horizontal')
-    
-    heatmap.ax_row_dendrogram.set_visible(False)
-    heatmap.ax_col_dendrogram.set_visible(True)
 
-    heatmap.savefig('SNP_matrix.pdf')
-    heatmap.savefig('SNP_matrix.png')
+    plt.title('SNP matrix visualized via HeatCluster')
+
+    heatmap.ax_row_dendrogram.set_visible(False)
+    heatmap.ax_col_dendrogram.set_visible(False)
+
+    heatmap.savefig(args.out + '.' + args.type)
 
     plt.show()
-    print("Done")
+    logging.info('Done')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# HeatCluster
-Creates a heatmap with an accompanying clustermap from a SNP matrix.
+# HeatCluster SNP Matrix Visualization
 
-https://github.com/DrB-S/HeatCluster/blob/main/SNP_matrix.pdf
+Welcome to HeatCluster SNP Matrix Visualization, a Python tool for visualizing SNP (Single Nucleotide Polymorphism) matrices generated via [SNP-dists](https://github.com/tseemann/snp-dists) using heatmaps. 
+
+## Introduction
+
+HeatCluster SNP Matrix Visualization is designed to provide an easy and effective way to visualize SNP matrices. By generating heatmaps from these SNP matrices, it can be easier to identify clusters.
+
+## Getting Started
+
+Follow these steps to get started with HeatCluster SNP Matrix Visualization:
+
+1. Clone the HeatCluster SNP Matrix Visualization repository to your local machine:
+
+```bash
+git clone https://github.com/DrB-S/HeatCluster.git
+```
+
+`HeatCluster.py` can be found in the HeatCluster directory that was just created.
+
+## Running HeatCluster.py
+
+```
+usage: HeatCluster.py [-h] -i INPUT [-o OUT] [-t TYPE] [-v]
+
+options:
+  -h, --help                show this help message and exit
+  -i INPUT, --input INPUT   input SNP matrix
+  -o OUT, --out OUT         final file name (default = 'SNP_matrix')
+  -t TYPE, --type TYPE      file extention for final image (default = 'pdf')
+  -v, --version             print version and exit
+```
+
+Example:
+```
+./HeatCluster.py -i test/snp_matrix.txt -o test
+```
+
+This will generate a file called 'test.pdf'.
+
+Examples:
+
+![alt text](assets/test.png)
+
+![alt text](assets/small_test.png)
+
+![alt text](assets/med_test.png)
