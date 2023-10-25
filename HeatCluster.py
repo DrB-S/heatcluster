9fab747a8ecf8a9312a79eef8d505b1a13c973cb
Stephen Beckstrom-Sternberg <Stephen.Beckstrom-Sternberg@azdhs.gov>
2023-10-25 14:55:23 -0700

    Update HeatCluster.py
    Added import scipy to prevent failing test

--- HeatCluster.py
+++ HeatCluster.py
@@ -14,6 +14,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy
 from pathlib import Path
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%y-%b-%d %H:%M:%S', level=logging.INFO)
